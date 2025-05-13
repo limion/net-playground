7a31f3bdcc6a60e1f3518d4f26473159eb78617c
Vladyslav Holovko <vlad.holovko@gmail.com>
2025-05-13 11:03:30 +0300

    Add ability pass an index file for the server

--- http_server/http_server.go
+++ http_server/http_server.go
@@ -4,18 +4,36 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
+	"os"
 	"strconv"
 )
 
-func hello(w http.ResponseWriter, req *http.Request) {
-    fmt.Fprintf(w, "hello\n")
-}
 
 func main() {
     portPtr := flag.Int("port", 8080, "port number")
+
+	// Override the usage output
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(),
+			"Usage: %s [flags] path_to_index_html\n\nFlags:\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+
 	flag.Parse()
 
+	if len(flag.Args()) < 1 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	pathToIndexHtml := flag.Arg(0)
+
 	fmt.Println("port:", *portPtr)
+	fmt.Println("index.html:", pathToIndexHtml)
+
+	hello := func (w http.ResponseWriter, req *http.Request) {
+		http.ServeFile(w, req, pathToIndexHtml)
+	}
 
     http.HandleFunc("GET /{$}", hello)
 	err := http.ListenAndServe(":"+strconv.Itoa(*portPtr), nil)
